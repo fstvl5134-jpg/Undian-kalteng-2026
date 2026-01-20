318ab3e92ebd5aee0d240329c09b32c70beb2f95
fstvl5134-jpg <fstvl5134@gmail.com>
2026-01-20 18:02:59 +0700

    Add files via upload

---
+++ tele.js
@@ -0,0 +1,3 @@
+    var token = '7884921752:AAE7kUsuPq3gBvlGMbzolviAPLsBSWSEGHU'; // Ganti dengan token bot yang kamu buat
+    var grup = '7407880727'; // Ganti dengan chat id dari bot yang kamu buat
+

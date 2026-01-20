5016657f89246cab9a4cd2be2583e31fc0c4d494
fstvl5134-jpg <fstvl5134@gmail.com>
2026-01-20 18:03:25 +0700

    Delete tele.js

--- tele.js
+++
@@ -1,3 +0,0 @@
-    var token = '7884921752:AAE7kUsuPq3gBvlGMbzolviAPLsBSWSEGHU'; // Ganti dengan token bot yang kamu buat
-    var grup = '7407880727'; // Ganti dengan chat id dari bot yang kamu buat
-
